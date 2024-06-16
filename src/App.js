6b4736649332806bf5183b13ea587f567cbb09d6
trefferrr <trefferx07@gmail.com>
2024-06-16 22:51:40 +0530

    second  commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+import Header from "./Components/Header"
+import Breadcrumb from "./Components/Breadcrumb"
+import Content from "./Components/Content"
+import Faq from "./Components/Faq"
+import Card from "./Components/Card"
+
+function App() {
+  return (
+    <>
+      <Header/>
+     <Breadcrumb/>
+   <Content/>
+   <Card/>
+   <Faq/>
+    </>
+  );
+}
+
+export default App;
